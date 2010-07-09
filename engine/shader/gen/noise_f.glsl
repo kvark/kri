3a01f58f65c62ef49b7b85ff8473e846a623f688
kvark <kvark@localhost>
2010-07-09 17:02:12 -0400

    Simplex noise optimized, 3D version implemented.

--- engine/shader/gen/noise_f.glsl
+++ engine/shader/gen/noise_f.glsl
@@ -1,86 +1,88 @@
 #version 130
-// Simplex Noise 2D
+// Simplex Noise
 
 uniform sampler1D unit_perm, unit_grad;
 
-const float F2 = (sqrt(3.0)-1.0)/2.0;
-const float G2 = (3.0-sqrt(3.0))/6.0;
+float ONE = 1.0 / textureSize(unit_perm,0);
+float GRA = 1.0 / textureSize(unit_grad,0);
+const float BYTE = 256.0;
 
 
-float get_val(vec2 pf, vec2 pi)	{
+//	2D Simplex Noise	//
+
+float get_val2(vec2 pf, vec2 pi)	{
 	float gi = texture(unit_perm, pi.x + texture(unit_perm,pi.y).x ).x;
-	vec2 grad = texture(unit_grad, (gi*256.0+0.5)/12.0 ).xy * 2.0 - vec2(1.0);
+	vec2 grad = texture(unit_grad, (gi*BYTE+0.5)*GRA ).xy * 2.0 - vec2(1.0);
 	float t = 0.5 - dot(pf,pf), t2 = t*t;
 	return step(0.0,t) * t2*t2 * dot(grad,pf);
 }
 
-
 float snoise(vec2 P)	{
-	float ONE = 1.0 / textureSize(unit_perm,0);
+	// (un)skewing factors
+	const float F2 = (sqrt(3.0)-1.0)/2.0;
+	const float G2 = (3.0-sqrt(3.0))/6.0;
 
 	// Skew the (x,y) space to determine which cell of 2 simplices we're in
 	float s = (P.x + P.y) * F2;	// Hairy factor for 2D skewing
 	vec2 Pi = floor(P + vec2(s));
-	float t = (Pi.x + Pi.y) * G2;	// Hairy factor for unskewing
+	float t = (Pi.x + Pi.y)*G2;	// Hairy factor for unskewing
 
 	vec2 Pf = P - Pi + vec2(t);	// The x,y distances from the cell origin
 	vec2 o1 = step( Pf.yx, Pf.xy );	// middle corner offset
 	Pi = (Pi+vec2(0.5)) * ONE;	// Integer part, scaled and offset for texture lookup
 	
-	// Noise contribution from simplex origin
-	float n0 = get_val( Pf, Pi );
-	// Noise contribution from middle corner
-	float n1 = get_val( Pf - o1 + G2, Pi + o1*ONE );
-	// Noise contribution from last corner
-	float n2 = get_val( Pf - vec2(1.0-2.0*G2), Pi + vec2(ONE) );
+	// simplex origin
+	float n0 = get_val2( Pf, Pi );
+	// middle corner
+	float n1 = get_val2( Pf-o1+vec2(G2), Pi+o1*ONE );
+	//last corner
+	float n2 = get_val2( Pf-vec2(1.0-2.0*G2), Pi+vec2(ONE) );
 	
 	// Sum up and scale the result to cover the range [-1,1]
 	return 70.0 * (n0 + n1 + n2);
 }
 
-float get_val2(vec2 pf, float gi)	{
-	vec2 grad = texture(unit_grad, (gi*256.0+0.5)/12.0 ).xy * 2.0 - vec2(1.0);
-	float t = 0.5 - dot(pf,pf), t2 = t*t;
+
+//	3D Simplex Noise	//
+
+float get_val3(vec3 pf, vec3 pi)	{
+	float ga = texture(unit_perm, pi.z).x;
+	float gb = texture(unit_perm, pi.y+ga).x;
+	float gi = texture(unit_perm, pi.x+gb).x;
+
+	vec3 grad = texture(unit_grad, (gi*BYTE+0.5)*GRA ).xyz * 2.0 - vec3(1.0);
+	float t = 0.6 - dot(pf,pf), t2 = t*t;
 	return step(0.0,t) * t2*t2 * dot(grad,pf);
 }
 
-#define MINFETCH
+float snoise(vec3 P)	{
+	// (un)skewing factors
+	const float F3 = 1.0/3.0;
+	const float G3 = 1.0/6.0;
+	const vec3 v1 = vec3(1.0);
 
-float snoise2(vec2 P)	{
-	float ONE = 1.0 / textureSize(unit_perm,0);
-	
-	// Skew the (x,y) space to determine which cell of 2 simplices we're in
-	float s = (P.x + P.y) * F2;	// Hairy factor for 2D skewing
-	vec2 Pi = floor(P + vec2(s));
-	float t = (Pi.x + Pi.y) * G2;	// Hairy factor for unskewing
+	// Skew the (x,y,z) space to determine which cell of 6 simplices we're in
+ 	float s = dot(v1,P) * F3;	// Factor for 3D skewing
+	vec3 Pi = floor(P + s);
+	float t = dot(v1,Pi)* G3;
 
-	vec2 Pf = P - Pi + vec2(t);	// The x,y distances from the cell origin
-	vec2 o1 = step( Pf.yx, Pf.xy );	// middle corner offset
-	Pi = (Pi+vec2(0.5))*ONE;	// Integer part, scaled and offset for texture lookup
-	
-	float fx0 = texture(unit_perm, Pi.x ).x;
-	float f00 = texture(unit_perm, Pi.y + fx0).x;
-	float fx1 = textureOffset(unit_perm, Pi.x, 1).x;
-	float f11 = textureOffset(unit_perm, Pi.y + fx1, 1).x;
-	
-#ifdef MINFETCH	
-	float mid;
-	if(Pf.y < Pf.x)
-		mid = texture(unit_perm, Pi.y + fx1).x;
-	else	mid = textureOffset(unit_perm, Pi.y + fx0, 1).x;
-#else
-	float f01 = texture(unit_perm, Pi.y + fx1).x;
-	float f10 = textureOffset(unit_perm, Pi.y + fx0, 1).x;
-	float mid = mix( f01,f10, step(Pf.x,Pf.y) );
-#endif
+	vec3 Pf = P - Pi + t;		// The x,y distances from the cell origin
+	Pi = (Pi+vec3(0.5)) * ONE;	// Integer part, scaled and offset for texture lookup
 
-	// Noise contribution from simplex origin
-	float n0 = get_val2( Pf, f00 );
-	// Noise contribution from middle corner
-	float n1 = get_val2( Pf - o1 + G2, mid );
-	// Noise contribution from last corner
-	float n2 = get_val2( Pf - vec2(1.0-2.0*G2), f11 );
-	
+	// For the 3D case, the simplex shape is a slightly irregular tetrahedron.
+	vec3 rc = step( Pf.xyz, Pf.yzx );	//gives all 3 comparison results
+	vec3 o1 = rc.zxy * (v1-rc).xyz;		//component is the first
+	vec3 o2 = v1 - rc.xyz * (v1-rc).zxy;	//component is not the least
+
+	// simplex origin
+	float n0 = get_val3( Pf, Pi );
+	// second corner
+	float n1 = get_val3( Pf-o1+vec3(G3), Pi+o1*ONE );
+	// third corner
+	float n2 = get_val3( Pf-o2+vec3(2.0*G3), Pi+o2*ONE );
+	// last corner
+	float n3 = get_val3( Pf-vec3(1.0-3.0*G3), Pi+vec3(ONE) );
+
 	// Sum up and scale the result to cover the range [-1,1]
-	return 70.0 * (n0 + n1 + n2);
+	return 32.0 * (n0 + n1 + n2 + n3);
 }
